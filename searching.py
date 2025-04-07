1233aca74f4a997668e7b2dc84996bc76a80d5ae
Vlastimil Spanihel <vlastik.sp@gmail.com>
2025-04-07 09:04:41 +0200

    commit 1

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -11,12 +12,25 @@ def read_data(file_name, field):
     :param field: (str), field of a dict to return
     :return: (list, string),
     """
+    #file_path = os.path.join(cwd_path, file_name)
+
+    if field not in {"unordered_numbers", "ordered_numbers", "dna_sequence"}:
+        return None
+
     file_path = os.path.join(cwd_path, file_name)
+
+    with open(file_path, "r") as json_file:
+        sequentional_data = json.load(json_file)
+
+    return sequentional_data
+
 
 
 def main():
-    pass
-
+    file_name = "sequential.json"
+    #raed data
+    seq = read_data(file_name, field="unordered_numbers")
+    print(seq)
 
 if __name__ == '__main__':
     main()
